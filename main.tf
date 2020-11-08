d172fae59114b5aca0b972ece0aea8cdf64006e0
eugene proshchenko <23uk@tut.by>
2020-11-08 19:54:47 +0300

    Fix 89

--- main.tf
+++ main.tf
@@ -2,14 +2,35 @@ data "hcloud_ssh_key" "devops" {
   name = "REBRAIN.SSH.PUB.KEY"
 }
 
+resource "hcloud_ssh_key" "a23uk_key" {
+  name       = "a23uk_key"
+  public_key = var.a23uk_key
+}
+
+
 resource "hcloud_server" "devops_23uk" {
   count    = length(var.domains)
   image    = "debian-9"
   name     = element(var.domains, count.index)
   server_type = "cx11"
   location   = "fsn1"
-  ssh_keys =  [data.hcloud_ssh_key.devops.id]
+  ssh_keys = [data.hcloud_ssh_key.devops.id,hcloud_ssh_key.a23uk_key.id]
+connection {
+  type = "ssh"
+  host = self.ipv4_address
+  user = "root"
+  private_key = file(var.connection.private_key)
+  } 
+
+  provisioner "remote-exec" {
+    inline = [
+"echo root:${var.pass} | chpasswd"
+    ]
+  }
+
+
 }
+
 
 data "aws_route53_zone" "dns" {
   name         = "devops.rebrain.srwx.net."
@@ -22,10 +43,6 @@ resource "aws_route53_record" "dns_rebrain" {
   name    = "23uk-${element(var.domains, count.index)}.devops.rebrain.srwx.net"
   type    = "A"
   ttl     = "30"
-#  records = [data.hcloud_server.serverinfo.ipv4_address]
   records = [element(hcloud_server.devops_23uk[*].ipv4_address, count.index)]
 }
 
-#output "aws_route53_zone" {
-#  value = data.aws_route53_zone.dns.zone_id
-#}
--- variables.tf
+++ variables.tf
@@ -3,6 +3,11 @@ type=string
 }
 variable "a23uk_key" {
 type=string
+#default = "~/.ssh/id_rsa.pub"
+}
+
+variable "rebrain_key" {
+type=string
 }
 
 variable "aws_access_key" {
@@ -13,13 +18,19 @@ variable "aws_secret_key" {
 type=string
 }
 
-#variable "zone_id" {
-#type=string
-#}
-
 variable "domains" {
 type=list
 }
 
+variable "pass" {
+type=string
+}
 
+variable "connection" {
+  default = {
+    user        = "root"
+    type        = "ssh"
+    private_key = "~/.ssh/id_rsa"
+  }
+}
 
